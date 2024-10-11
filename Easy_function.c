6084606f088518265b1a2cd27d8470beb0cca7b8
DateTiro <a24i336jl@ous.jp>
2024-10-11 15:18:43 +0900

    rename

---
+++ Easy_function.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int func(int a, int b);
+
+int main(void) {
+    int a, b, c;
+    printf("Please insert intergers.\n");
+    printf("number1:");
+    scanf("%d", &a);
+    printf("number2:");
+    scanf("%d", &b);
+    c = func(a, b);
+    printf("%d * %d = %d", a, b, c);
+
+    return 0;
+}
+
+int func(int x, int y) {
+    int z;
+    z = x * y;
+
+    return z;
+}
